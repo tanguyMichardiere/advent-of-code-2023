8b5de2ca1e9341ddbc06de0e60c86ce2a14827e4
Tanguy Michardière <54041658+tanguyMichardiere@users.noreply.github.com>
2023-12-16 23:38:52 +0100

    refactor for readability and speed

--- src/day16.rs
+++ src/day16.rs
@@ -1,16 +1,13 @@
-#[derive(Clone)]
 enum Mirror {
     Right, // /
     Left,  // \
 }
 
-#[derive(Clone)]
 enum Splitter {
     Vertical,   // |
     Horizontal, // -
 }
 
-#[derive(Clone)]
 enum Object {
     Mirror(Mirror),
     Splitter(Splitter),
@@ -85,95 +82,111 @@ impl Direction {
     }
 }
 
-type Grid = Vec<Vec<(Option<Object>, Vec<Direction>)>>;
-
-fn parse(input: &str) -> Grid {
-    input
-        .lines()
-        .map(|line| {
-            line.chars()
-                .map(|char| {
-                    (
-                        match char {
-                            '.' => None,
-                            '/' => Some(Object::Mirror(Mirror::Right)),
-                            '\\' => Some(Object::Mirror(Mirror::Left)),
-                            '|' => Some(Object::Splitter(Splitter::Vertical)),
-                            '-' => Some(Object::Splitter(Splitter::Horizontal)),
-                            _ => unreachable!(),
-                        },
-                        Vec::with_capacity(4),
-                    )
-                })
-                .collect()
-        })
-        .collect()
+struct Tile {
+    object: Option<Object>,
+    beams: Vec<Direction>,
 }
 
-fn advance_beam(grid: &mut Grid, (x, y): Coordinates, direction: Direction) {
-    let size = grid.len();
-    if !grid[y][x].1.contains(&direction) {
-        grid[y][x].1.push(direction.clone());
-        match &grid[y][x].0 {
-            None => {
-                if let Some((x, y)) = direction.next((x, y), size) {
-                    advance_beam(grid, (x, y), direction);
+impl From<char> for Tile {
+    fn from(value: char) -> Self {
+        Self {
+            object: match value {
+                '.' => None,
+                '/' => Some(Object::Mirror(Mirror::Right)),
+                '\\' => Some(Object::Mirror(Mirror::Left)),
+                '|' => Some(Object::Splitter(Splitter::Vertical)),
+                '-' => Some(Object::Splitter(Splitter::Horizontal)),
+                _ => unreachable!(),
+            },
+            beams: Vec::with_capacity(4),
+        }
+    }
+}
+
+struct Grid {
+    tiles: Vec<Vec<Tile>>,
+    size: usize,
+}
+
+impl Grid {
+    fn parse(input: &str) -> Self {
+        let tiles = input
+            .lines()
+            .map(|line| line.chars().map(Tile::from).collect::<Vec<_>>())
+            .collect::<Vec<_>>();
+        let size = tiles.len();
+        Self { tiles, size }
+    }
+
+    fn advance_beam(&mut self, coordinates: Coordinates, direction: Direction) {
+        let Tile { object, beams } = &mut self.tiles[coordinates.1][coordinates.0];
+        if !beams.contains(&direction) {
+            beams.push(direction.clone());
+            match &object {
+                None => {
+                    if let Some(coordinates) = direction.next(coordinates, self.size) {
+                        self.advance_beam(coordinates, direction);
+                    }
                 }
-            }
-            Some(Object::Mirror(mirror)) => {
-                let direction = direction.reflect(mirror);
-                if let Some((x, y)) = direction.next((x, y), size) {
-                    advance_beam(grid, (x, y), direction);
+                Some(Object::Mirror(mirror)) => {
+                    let direction = direction.reflect(mirror);
+                    if let Some(coordinates) = direction.next(coordinates, self.size) {
+                        self.advance_beam(coordinates, direction);
+                    }
                 }
-            }
-            Some(Object::Splitter(splitter)) => {
-                for direction in direction.split(splitter) {
-                    if let Some((x, y)) = direction.next((x, y), size) {
-                        advance_beam(grid, (x, y), direction);
+                Some(Object::Splitter(splitter)) => {
+                    for direction in direction.split(splitter) {
+                        if let Some(coordinates) = direction.next(coordinates, self.size) {
+                            self.advance_beam(coordinates, direction);
+                        }
                     }
                 }
             }
         }
     }
-}
 
-fn energy(grid: &Grid) -> usize {
-    grid.iter()
-        .map(|row| row.iter().filter(|(_, beams)| !beams.is_empty()).count())
-        .sum()
+    fn energy(&self) -> usize {
+        self.tiles
+            .iter()
+            .map(|row| {
+                row.iter()
+                    .filter(|Tile { beams, .. }| !beams.is_empty())
+                    .count()
+            })
+            .sum()
+    }
+
+    fn reset(&mut self) {
+        for row in &mut self.tiles {
+            for tile in row {
+                tile.beams.truncate(0);
+            }
+        }
+    }
 }
 
 pub fn part_one(input: &str) -> usize {
-    let mut grid = parse(input);
-    advance_beam(&mut grid, (0, 0), Direction::Right);
-    energy(&grid)
+    let mut grid = Grid::parse(input);
+    grid.advance_beam((0, 0), Direction::Right);
+    grid.energy()
 }
 
 pub fn part_two(input: &str) -> usize {
-    let grid = parse(input);
-    let size = grid.len();
+    let mut grid = Grid::parse(input);
     let mut max_energy = 0;
-    for i in 0..size {
-        {
-            let mut grid = grid.clone();
-            advance_beam(&mut grid, (0, i), Direction::Right);
-            max_energy = max_energy.max(energy(&grid));
-        }
-        {
-            let mut grid = grid.clone();
-            advance_beam(&mut grid, (i, size - 1), Direction::Up);
-            max_energy = max_energy.max(energy(&grid));
-        }
-        {
-            let mut grid = grid.clone();
-            advance_beam(&mut grid, (size - 1, i), Direction::Left);
-            max_energy = max_energy.max(energy(&grid));
-        }
-        {
-            let mut grid = grid.clone();
-            advance_beam(&mut grid, (i, 0), Direction::Down);
-            max_energy = max_energy.max(energy(&grid));
-        }
+    for i in 0..grid.size {
+        grid.advance_beam((0, i), Direction::Right);
+        max_energy = max_energy.max(grid.energy());
+        grid.reset();
+        grid.advance_beam((i, grid.size - 1), Direction::Up);
+        max_energy = max_energy.max(grid.energy());
+        grid.reset();
+        grid.advance_beam((grid.size - 1, i), Direction::Left);
+        max_energy = max_energy.max(grid.energy());
+        grid.reset();
+        grid.advance_beam((i, 0), Direction::Down);
+        max_energy = max_energy.max(grid.energy());
+        grid.reset();
     }
     max_energy
 }
