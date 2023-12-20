802b700967b610698722b9119bad1056ebcd1617
Tanguy Michardière <54041658+tanguyMichardiere@users.noreply.github.com>
2023-12-20 17:17:10 +0100

    use IDs instead of strings

--- src/day20.rs
+++ src/day20.rs
@@ -1,15 +1,16 @@
 use std::collections::HashMap;
-use std::iter;
 use std::str::FromStr;
 
+use crate::cache::hash;
 use crate::regex;
 
 struct State {
-    modules: HashMap<String, Module>,
+    modules: HashMap<u64, Module>,
     low_pulse_count: usize,
     high_pulse_count: usize,
-    before_rx: String,
-    cycle_lens: HashMap<String, Option<usize>>,
+    before_rx: Option<u64>,
+    // frequency at which the module before rx receives a high pulse from each of its input modules
+    cycles: HashMap<u64, Option<usize>>,
 }
 
 impl FromStr for State {
@@ -22,115 +23,144 @@ impl FromStr for State {
                 .map(|caps| {
                     let outputs = regex!(r"(?P<name>[a-z]+)")
                         .captures_iter(&caps["outputs"])
-                        .map(|caps| caps["name"].to_owned())
+                        .map(|caps| hash(&caps["name"]))
                         .collect();
                     match &caps["name"] {
-                        "broadcaster" => (
-                            "broadcaster".to_owned(),
-                            Module::Broadcaster(BroadcasterModule { outputs }),
-                        ),
-                        name if name.starts_with("%") => (
-                            name[1..].to_owned(),
-                            Module::FlipFlop(FlipFlopModule { on: false, outputs }),
-                        ),
-                        name if name.starts_with("&") => (
-                            name[1..].to_owned(),
-                            Module::Conjunction(ConjunctionModule {
-                                memory: HashMap::new(),
-                                outputs,
-                            }),
-                        ),
+                        "broadcaster" => {
+                            let id = crate::cache::hash("broadcaster");
+                            (id, Module::Broadcaster(BroadcasterModule { id, outputs }))
+                        }
+                        name if name.starts_with("%") => {
+                            let id = crate::cache::hash(&name[1..]);
+                            (
+                                id,
+                                Module::FlipFlop(FlipFlopModule {
+                                    id,
+                                    on: false,
+                                    outputs,
+                                }),
+                            )
+                        }
+                        name if name.starts_with("&") => {
+                            let id = crate::cache::hash(&name[1..]);
+                            (
+                                id,
+                                Module::Conjunction(ConjunctionModule {
+                                    id,
+                                    memory: HashMap::new(),
+                                    outputs,
+                                }),
+                            )
+                        }
                         _ => unreachable!(),
                     }
                 })
                 .collect::<HashMap<_, _>>();
-        let mut empty_modules = Vec::new();
         let mut conjunction_inputs = HashMap::new();
         for (id, module) in &modules {
             for output in module.outputs() {
-                match modules.get(output) {
-                    None => {
-                        empty_modules.push(output.clone());
-                    }
-                    Some(Module::Conjunction(_)) => {
-                        conjunction_inputs
-                            .entry(output.clone())
-                            .or_insert(Vec::new())
-                            .push(id.clone());
-                    }
-                    _ => {}
+                if let Some(Module::Conjunction(_)) = modules.get(output) {
+                    conjunction_inputs
+                        .entry(*output)
+                        .or_insert(Vec::new())
+                        .push(*id);
                 }
             }
-        }
-        for empty_module in empty_modules {
-            modules.insert(empty_module, Module::Empty);
         }
         for (name, inputs) in conjunction_inputs {
             if let Module::Conjunction(conjunction) = modules.get_mut(&name).unwrap() {
                 for input in inputs {
-                    conjunction.memory.insert(input.clone(), false);
+                    conjunction.memory.insert(input, false);
                 }
             }
         }
         let before_rx = modules
             .iter()
             .find_map(|(id, module)| {
-                if module.outputs().any(|output| output == "rx") {
+                if module.outputs().iter().any(|output| output == &hash("rx")) {
                     Some(id)
                 } else {
                     None
                 }
             })
-            .unwrap()
-            .clone();
-        let cycle_lens = modules
-            .keys()
-            .filter_map(|id| {
-                if modules[id].outputs().any(|output| output == &before_rx) {
-                    Some((id.clone(), None))
-                } else {
-                    None
-                }
-            })
-            .collect::<HashMap<_, _>>();
+            .cloned();
+        let cycle_lens = if let Some(before_rx) = before_rx {
+            modules
+                .keys()
+                .filter_map(|id| {
+                    if modules[id]
+                        .outputs()
+                        .iter()
+                        .any(|output| output == &before_rx)
+                    {
+                        Some((*id, None))
+                    } else {
+                        None
+                    }
+                })
+                .collect::<HashMap<_, _>>()
+        } else {
+            HashMap::new()
+        };
         Ok(Self {
             modules,
             low_pulse_count: 0,
             high_pulse_count: 0,
             before_rx,
-            cycle_lens,
+            cycles: cycle_lens,
         })
     }
 }
 
 impl State {
-    fn resolve(&mut self, pulses: impl Iterator<Item = Pulse>, count: usize) {
+    fn push_button(&mut self, count: usize) {
+        self.resolve(
+            vec![(
+                Pulse {
+                    from: 0,
+                    high: false,
+                },
+                vec![hash("broadcaster")],
+            )],
+            count,
+        );
+    }
+
+    fn resolve(&mut self, pulses: Vec<(Pulse, Vec<u64>)>, count: usize) {
         let mut next_pulses = Vec::new();
-        for pulse in pulses {
+        for (pulse, outputs) in pulses {
             if pulse.high {
-                self.high_pulse_count += 1;
+                self.high_pulse_count += outputs.len();
             } else {
-                self.low_pulse_count += 1;
+                self.low_pulse_count += outputs.len();
             }
-            if pulse.to == self.before_rx && pulse.high {
-                *self.cycle_lens.get_mut(&pulse.from).unwrap() = Some(count);
+            for output in outputs {
+                if self.before_rx.is_some_and(|before_rx| output == before_rx) && pulse.high {
+                    *self.cycles.get_mut(&pulse.from).unwrap() = Some(count);
+                }
+                if let Some(module) = self.modules.get_mut(&output) {
+                    if let Some(pulse) = module.resolve(&pulse) {
+                        next_pulses.push((pulse, module.outputs().to_vec()));
+                    }
+                }
             }
-            next_pulses.extend(self.modules.get_mut(&pulse.to).unwrap().resolve(&pulse));
         }
         if !next_pulses.is_empty() {
-            self.resolve(next_pulses.into_iter(), count);
+            self.resolve(next_pulses, count);
         }
     }
 
-    fn push_button(&mut self, count: usize) {
-        self.resolve(
-            iter::once(Pulse {
-                from: "button".to_owned(),
-                to: "broadcaster".to_owned(),
-                high: false,
-            }),
-            count,
-        );
+    fn cycles_to_activate(&self) -> Option<usize> {
+        if self.cycles.values().all(|cycle_len| cycle_len.is_some()) {
+            Some(
+                self.cycles
+                    .values()
+                    .map(|cycle_len| cycle_len.unwrap())
+                    .product(),
+            )
+        } else {
+            None
+        }
     }
 }
 
@@ -138,83 +168,78 @@ enum Module {
     Broadcaster(BroadcasterModule),
     FlipFlop(FlipFlopModule),
     Conjunction(ConjunctionModule),
-    Empty,
 }
 
 impl Module {
-    fn outputs<'a>(&'a self) -> Box<dyn Iterator<Item = &'a String> + 'a> {
+    fn outputs(&self) -> &[u64] {
         match self {
-            Self::Broadcaster(broadcaster_module) => Box::new(broadcaster_module.outputs.iter()),
-            Self::FlipFlop(flip_flop_module) => Box::new(flip_flop_module.outputs.iter()),
-            Self::Conjunction(conjunction_module) => Box::new(conjunction_module.outputs.iter()),
-            Self::Empty => Box::new(iter::empty()),
+            Self::Broadcaster(broadcaster_module) => &broadcaster_module.outputs,
+            Self::FlipFlop(flip_flop_module) => &flip_flop_module.outputs,
+            Self::Conjunction(conjunction_module) => &conjunction_module.outputs,
         }
     }
 
-    fn resolve<'a>(&'a mut self, pulse: &'a Pulse) -> Box<dyn Iterator<Item = Pulse> + '_> {
+    fn resolve(&mut self, pulse: &Pulse) -> Option<Pulse> {
         match self {
-            Self::Broadcaster(broadcaster) => Box::new(broadcaster.resolve(pulse)),
-            Self::FlipFlop(flip_flop) => Box::new(flip_flop.resolve(pulse)),
-            Self::Conjunction(conjunction) => Box::new(conjunction.resolve(pulse)),
-            Self::Empty => Box::new(iter::empty()),
+            Self::Broadcaster(broadcaster) => Some(broadcaster.resolve(pulse)),
+            Self::FlipFlop(flip_flop) => flip_flop.resolve(pulse),
+            Self::Conjunction(conjunction) => Some(conjunction.resolve(pulse)),
         }
     }
 }
 
 struct BroadcasterModule {
-    outputs: Vec<String>,
+    id: u64,
+    outputs: Vec<u64>,
 }
 
 impl BroadcasterModule {
-    fn resolve<'a>(&'a self, pulse: &'a Pulse) -> impl Iterator<Item = Pulse> + '_ {
-        self.outputs.iter().map(move |output| Pulse {
-            from: pulse.to.clone(),
-            to: output.to_owned(),
+    fn resolve(&self, pulse: &Pulse) -> Pulse {
+        Pulse {
+            from: self.id,
             high: pulse.high,
-        })
+        }
     }
 }
 
 struct FlipFlopModule {
+    id: u64,
     on: bool,
-    outputs: Vec<String>,
+    outputs: Vec<u64>,
 }
 
 impl FlipFlopModule {
-    fn resolve<'a>(&'a mut self, pulse: &'a Pulse) -> Box<dyn Iterator<Item = Pulse> + '_> {
+    fn resolve(&mut self, pulse: &Pulse) -> Option<Pulse> {
         if !pulse.high {
             self.on = !self.on;
-            Box::new(self.outputs.iter().map(|output| Pulse {
-                from: pulse.to.clone(),
-                to: output.to_owned(),
+            Some(Pulse {
+                from: self.id,
                 high: self.on,
-            }))
+            })
         } else {
-            Box::new(iter::empty())
+            None
         }
     }
 }
 
 struct ConjunctionModule {
-    memory: HashMap<String, bool>,
-    outputs: Vec<String>,
+    id: u64,
+    memory: HashMap<u64, bool>,
+    outputs: Vec<u64>,
 }
 
 impl ConjunctionModule {
-    fn resolve<'a>(&'a mut self, pulse: &'a Pulse) -> impl Iterator<Item = Pulse> + '_ {
+    fn resolve(&mut self, pulse: &Pulse) -> Pulse {
         *self.memory.get_mut(&pulse.from).unwrap() = pulse.high;
-        let high = !self.memory.values().all(|high| *high);
-        self.outputs.iter().map(move |output| Pulse {
-            from: pulse.to.clone(),
-            to: output.to_owned(),
-            high,
-        })
+        Pulse {
+            from: self.id,
+            high: !self.memory.values().all(|high| *high),
+        }
     }
 }
 
 struct Pulse {
-    from: String,
-    to: String,
+    from: u64,
     high: bool,
 }
 
@@ -232,16 +257,8 @@ pub fn part_two(input: &str) -> usize {
     loop {
         count += 1;
         state.push_button(count);
-        if state
-            .cycle_lens
-            .values()
-            .all(|cycle_len| cycle_len.is_some())
-        {
-            break state
-                .cycle_lens
-                .into_values()
-                .map(|cycle_len| cycle_len.unwrap())
-                .product();
+        if let Some(cycles_to_activate) = state.cycles_to_activate() {
+            break cycles_to_activate;
         }
     }
 }
